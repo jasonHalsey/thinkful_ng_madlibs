f121e3ba77c13bccdb6beddf73e7fb853369a2dd
Jason Halsey <jasonhalsey@01260-MBP15R.local>
2016-10-18 10:14:30 -0700

    Add controller as

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
-var myapp = angular.module('madapp', []);
-myapp.controller('mycntrl', function($scope) {
-  $scope.malePronoun = "he";
-  $scope.femalePronoun = "she";
+
+
+angular.module('madapp', []).controller('mycntrl', function(){
+    var vm = this;
+    vm.malePronoun = "he";
+    vm.femalePronoun = "she";
+    vm.male_name = "Male Name";
+    vm.job_title = "Job Title"
+    vm.tedious_task = "Tedious Task";
+    vm.dirty_task = "Dirty Task";
+    vm.celebrity = "Celebrity";
+    vm.useless_skill = "Useless Skill";
+    vm.obnoxious_celebrity = "Obnoxious Celebrity";
+    vm.huge_number = "Huge Number";
+    vm.adjective = "Adjective";
 });
