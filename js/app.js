a74fa57925c769685f0015bb38dc8df6d5caf62c
Jason Halsey <jasonhalsey@01260-MBP15R.local>
2016-10-19 11:26:23 -0700

    Add show/hide to app

--- js/app.js
+++ js/app.js
@@ -1,16 +1,4 @@
-
-
-angular.module('madapp', []).controller('mycntrl', function(){
+angular.module('madapp', ['ngAnimate']).controller('mycntrl', function(){
     var vm = this;
-    vm.malePronoun = "he";
-    vm.femalePronoun = "she";
-    vm.male_name = "Male Name";
-    vm.job_title = "Job Title"
-    vm.tedious_task = "Tedious Task";
-    vm.dirty_task = "Dirty Task";
-    vm.celebrity = "Celebrity";
-    vm.useless_skill = "Useless Skill";
-    vm.obnoxious_celebrity = "Obnoxious Celebrity";
-    vm.huge_number = "Huge Number";
-    vm.adjective = "Adjective";
+
 });
