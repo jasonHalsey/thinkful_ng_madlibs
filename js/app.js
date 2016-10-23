8ff6133ab6536d8613f7ff451544bd773c7f4afd
Jason Halsey <jasonhalsey@01260-MBP15R.local>
2016-10-23 16:38:52 -0700

    WIP:Reset Btn

--- js/app.js
+++ js/app.js
@@ -1,4 +1,41 @@
 angular.module('madapp', ['ngAnimate']).controller('mycntrl', function(){
-    var vm = this;
 
+    var vm = this;
+    // vm.reset = function reset(){
+    //     console.log('Click It');
+    //     myForm.$setPristine();
+    // }
+
+    var defaultForm = {
+        male_name : "",
+        job_title : "",
+        tedious_task : "",
+        dirty_task : "",
+        celebrity : "",
+        useless_skill : "",
+        obnoxious_celebrity : "",
+        huge_number : "",
+        adjective : ""
+    }
+
+    vm.user = angular.copy(defaultForm);
+
+
+    vm.reset = function reset() {
+        console.log('in');
+        vm.myForm.$setPristine();
+        vm.user = angular.copy(defaultForm);
+        console.log('empty');
+    }
+
+    // vm.reset = function ()
+    // {
+    //      console.log('balls');
+    //   myForm.$setPristine();
+
+    // };
+
+    vm.submit = function submit(){
+        console.log("Submit");
+    }
 });
