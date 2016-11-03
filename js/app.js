8ffc94ad9824753b39f8951cb754a873e9988986
Jason Halsey <jasonhalsey@01260-MBP15R.local>
2016-11-03 09:03:01 -0700

    Small js cleanup

--- js/app.js
+++ js/app.js
@@ -6,16 +6,6 @@ angular.module('madapp', ['ngAnimate']).controller('mycntrl', function(){
         console.log('Click It');
         vm.myForm = {}
     }
-
-
-
-    // vm.reset = function reset() {
-    //     console.log('in');
-    //     vm.myForm.$setPristine();
-    //     vm.user = angular.copy(defaultForm);
-    //     console.log('empty');
-    // }
-
     vm.submit = function submit(){
         console.log("Submit");
     }
